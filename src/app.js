d7462a3bf72a612333231530ac1ceda3fcd92554
unknown <srroy119@gmail.com>
2022-06-03 18:21:18 +0530

    second commit

---
+++ src/app.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require("cors");
+app.use(express.json());
+
+
+const allowedOrigins = ["http://localhost:3000","http://localhost:5000"];
+
+    app.use(
+        cors({
+            origin: function(origin, callback) {
+                if (!origin) return callback(null, true);
+                if (allowedOrigins.indexOf(origin) === -1) {
+                    var msg =
+                        "The CORS policy for this site does not " +
+                        "allow access from the specified Origin.";
+                    return callback(new Error(msg), false);
+                }
+                return callback(null, true);
+            }
+        })
+    ); 
+
+// port
+const port = 5000;
+
+require('./db/conn');
+ 
+
+app.listen(port, ()=>{
+    console.log(`listening at port-${port}`);
+})
---
+++ src/routers/login.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const loginRouter = express.Router();
+
+
+loginRouter.post('/api/login', async (req, res)=>{
+    try {
+        
+    } catch (err) {
+        console.log(err);
+    }
+})
