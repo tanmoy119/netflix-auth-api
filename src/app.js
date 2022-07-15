88c6514e47752602d5db4ffb971fb69d3492d0f6
unknown <srroy119@gmail.com>
2022-07-15 08:48:52 +0530

    cors commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
-const cors = require("cors");
 app.use(express.json());
 const loginRouter = require("./routers/login");
 
