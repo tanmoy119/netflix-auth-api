e761f0899808a858c364de8ddb569f89c50aeffc
unknown <srroy119@gmail.com>
2022-07-21 23:35:00 +0530

    otp resend commit2

--- src/app.js
+++ src/app.js
@@ -5,23 +5,23 @@ app.use(express.json());
 const cors = require('cors');
 const loginRouter = require("./routers/login");
 
-//app.use(cors());
-const allowedOrigins = ["http://localhost:3000","http://localhost:5000"];
+app.use(cors());
+// const allowedOrigins = ["http://localhost:3000","http://localhost:5000"];
 
-    app.use(
-        cors({
-            origin: function(origin, callback) {
-                if (!origin) return callback(null, true);
-                if (allowedOrigins.indexOf(origin) === -1) {
-                    var msg =
-                        "The CORS policy for this site does not " +
-                        "allow access from the specified Origin.";
-                    return callback(new Error(msg), false);
-                }
-                return callback(null, true);
-            }
-        })
-    ); 
+//     app.use(
+//         cors({
+//             origin: function(origin, callback) {
+//                 if (!origin) return callback(null, true);
+//                 if (allowedOrigins.indexOf(origin) === -1) {
+//                     var msg =
+//                         "The CORS policy for this site does not " +
+//                         "allow access from the specified Origin.";
+//                     return callback(new Error(msg), false);
+//                 }
+//                 return callback(null, true);
+//             }
+//         })
+//     ); 
 
 
 const port = process.env.PORT || 5000;
